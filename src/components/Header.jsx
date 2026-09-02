0e6fa2d1b07aea29d8a865804f3eb54b4f4817e3
Puja mangalpalli <pmsm901@gmail.com>
2026-09-02 18:20:49 +0530

    adding header

---
+++ src/components/Header.jsx
@@ -0,0 +1,4 @@
+import { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
+import {FiMenu,FiSearch,FiUser,FiLogout,FiVideo} from "react-icons/fi";
+import { userAuth } from "../context/AuthContext";
